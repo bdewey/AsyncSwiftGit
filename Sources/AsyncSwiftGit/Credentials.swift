13532389565b3de94ac0af37823af16b02057fa4
Brian Dewey <bdewey@gmail.com>
2022-06-04 19:01:37 -0700

    Work with SSH in-memory keys

--- Sources/AsyncSwiftGit/Credentials.swift
+++ Sources/AsyncSwiftGit/Credentials.swift
@@ -30,17 +30,18 @@ internal func credentialsCallback(
   cred: UnsafeMutablePointer<UnsafeMutablePointer<git_cred>?>?,
   url: UnsafePointer<CChar>?,
   username: UnsafePointer<CChar>?,
-  _: UInt32,
-  payload: UnsafeMutableRawPointer? ) -> Int32 {
+  allowedOptions: UInt32,
+  payload: UnsafeMutableRawPointer?
+) -> Int32 {
 
   let result: Int32
 
   // Find username_from_url
   let name = username.map(String.init(cString:))
 
-    let fetchOptions = FetchOptions.fromPointer(payload!)
+  let fetchOptions = FetchOptions.fromPointer(payload!)
 
-    switch fetchOptions.credentials {
+  switch fetchOptions.credentials {
   case .default:
     result = git_cred_default_new(cred)
   case .sshAgent:
