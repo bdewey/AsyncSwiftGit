c66faf128e8c7351638d65d612e27267293b8487
Brian Dewey <bdewey@gmail.com>
2022-06-27 14:26:15 -0700

    Update static-libgit2 version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/bdewey/static-libgit2", from: "0.3.0"),
+    .package(url: "https://github.com/bdewey/static-libgit2", from: "0.4.0"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.1.0"),
   ],
   targets: [
